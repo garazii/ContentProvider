260bff44d948afb415641204d9538fd2acac0d43
garazii <garazi_19_2@hotmail.com>
2017-02-15 09:30:21 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dm2/contentprovider/Dialogo.java
@@ -0,0 +1,59 @@
+package com.example.dm2.contentprovider;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class Dialogo extends AppCompatActivity {
+
+    private EditText cliente;
+    private EditText telefono;
+    private EditText email;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialogo);
+
+        Button but= (Button) findViewById(R.id.butConfirmar);
+
+        /*cliente = (EditText)v.findViewById(R.id.cliente);
+        telefono = (EditText)v.findViewById(R.id.telefono);
+        email = (EditText)v.findViewById(R.id.email);*/
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cliente = (EditText)findViewById(R.id.cliente);
+                telefono = (EditText)findViewById(R.id.telefono);
+                email = (EditText)findViewById(R.id.email);
+
+                String cli=cliente.getText().toString();
+                String tel=telefono.getText().toString();
+                String mail=email.getText().toString();
+
+                Intent intent=new Intent(Dialogo.this,MainActivity.class);
+                intent.putExtra("cliente",cli);
+                intent.putExtra("telefono",tel);
+                intent.putExtra("email",mail);
+                setResult(RESULT_OK,intent);
+                finish();
+
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/dm2/contentprovider/MainActivity.java
@@ -0,0 +1,166 @@
+package com.example.dm2.contentprovider;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private Button btnInsertar;
+    private Button btnConsultar;
+    private Button btnEliminar;
+    private TextView txtResultados;
+    private String cliente;
+    private String telefono;
+    private String email;
+    private ContentResolver cr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Referencias a los controles
+        txtResultados = (TextView)findViewById(R.id.TxtResultados);
+        btnConsultar = (Button)findViewById(R.id.BtnConsultar);
+        btnInsertar = (Button)findViewById(R.id.BtnInsertar);
+        btnEliminar = (Button)findViewById(R.id.BtnEliminar);
+
+
+
+        btnConsultar.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                //Columnas de la tabla a recuperar
+                String[] projection = new String[] {
+                        ClientesProvider.Clientes._ID,
+                        ClientesProvider.Clientes.COL_NOMBRE,
+                        ClientesProvider.Clientes.COL_TELEFONO,
+                        ClientesProvider.Clientes.COL_EMAIL };
+
+                Uri clientesUri =  ClientesProvider.CONTENT_URI;
+
+                ContentResolver cr = getContentResolver();
+
+                //Hacemos la consulta
+                Cursor cur = cr.query(clientesUri,
+                        projection, //Columnas a devolver
+                        null,       //Condición de la query
+                        null,       //Argumentos variables de la query
+                        null);      //Orden de los resultados
+                if (cur.moveToFirst())
+                {
+                    String nombre;
+                    String telefono;
+                    String email;
+
+                    int colNombre = cur.getColumnIndex(ClientesProvider.Clientes.COL_NOMBRE);
+                    int colTelefono = cur.getColumnIndex(ClientesProvider.Clientes.COL_TELEFONO);
+                    int colEmail = cur.getColumnIndex(ClientesProvider.Clientes.COL_EMAIL);
+
+                    txtResultados.setText("");
+
+                    do
+                    {
+
+                        nombre = cur.getString(colNombre);
+                        telefono = cur.getString(colTelefono);
+                        email = cur.getString(colEmail);
+
+                        txtResultados.append(nombre + " - " + telefono + " - " + email + "\n");
+
+                    } while (cur.moveToNext());
+                }
+            }
+        });
+
+        btnInsertar.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+
+                Intent intent=new Intent(MainActivity.this,Dialogo.class);
+                startActivityForResult(intent,1);
+            }
+        });
+
+        btnEliminar.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                cr = getContentResolver();
+
+
+                final AlertDialog.Builder alertDialog1 = new AlertDialog.Builder(MainActivity.this);
+
+                alertDialog1.setTitle("Eliminar cliente");
+                //columnas de la tabla a recuperar
+
+                String[] projection=new String[]{
+                        ClientesProvider.Clientes._ID,
+                        ClientesProvider.Clientes.COL_NOMBRE};
+                Uri clientesUri=ClientesProvider.CONTENT_URI;
+
+                //hacemos la consulta
+
+                final Cursor c= cr.query(clientesUri,projection,null,null,null);
+
+                alertDialog1.setMultiChoiceItems(c,ClientesProvider.Clientes.COL_NOMBRE,ClientesProvider.Clientes.COL_NOMBRE, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        if(isChecked)
+                        {
+                            c.moveToFirst();
+                            cr.delete(ClientesProvider.CONTENT_URI,ClientesProvider.Clientes.COL_NOMBRE+"='"+
+                                    c.getString(which)+"'",null);
+                            Toast.makeText(MainActivity.this,c.getType(which)+" eliminado",Toast.LENGTH_LONG).show();
+                            dialog.cancel();
+                        }
+                    }
+                });
+                alertDialog1.create().show();
+               /* if(c.moveToFirst())
+                {
+                    String nombre;
+                    int colNombre=c.getColumnIndex(ClientesProvider.Clientes.COL_NOMBRE);
+                    do{
+                        nombre=c.getString(colNombre);
+                        adaptador.add(nombre);
+                    }while(c.moveToNext());
+                }*/
+
+            }
+        });
+
+    }
+    protected void onActivityResult(int requestCode,int resultCode,Intent data)
+    {
+        if(requestCode==1&&resultCode==RESULT_OK)
+        {
+            cliente = data.getExtras().getString("cliente");
+            telefono =data.getExtras().getString("telefono");
+            email = data.getExtras().getString("email");
+            ContentValues values = new ContentValues();
+            values.put(ClientesProvider.Clientes.COL_NOMBRE, cliente);
+            values.put(ClientesProvider.Clientes.COL_TELEFONO, telefono);
+            values.put(ClientesProvider.Clientes.COL_EMAIL, email);
+
+            ContentResolver cr = getContentResolver();
+
+            cr.insert(ClientesProvider.CONTENT_URI, values);
+        }
+    }
+}
